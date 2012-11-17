2c31c1233118d5b577fec7331ca1a9fcffd54e66
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2012-11-17 14:49:06 +0000

    Super basic filtering to a single recording MBID.

--- app/js/spotibrainz.js
+++ app/js/spotibrainz.js
@@ -8,6 +8,26 @@ function init()
     views = sp.require("sp://import/scripts/api/views");
 
     models.player.observe(models.EVENT.CHANGE, function(event) {
-          console.log('Something changed!');
+          if (event.data.curtrack == true){
+              eventchange();
+	  }
     });
+}
+
+function eventchange()
+{
+    trackData = models.player.track.data;
+    album = trackData.album.name;
+    year = trackData.album.year;
+    track = trackData.name;
+    number = trackData.trackNumber;
+    duration = trackData.duration;
+    artists = trackData.artists;
+    albumArtist = trackData.album.artist.name;
+
+    $.ajax({url: 'http://musicbrainz.org/ws/2/recording', 
+            data: {fmt:'xml', 
+                   query: 'recording:' + track + ' artist:' + albumArtist + ' release:' + album + ' date:' + year + ' number:' + number + ' dur:' + duration}, 
+            success: function(data) { console.log(data); var resp = $(data); console.log(resp.find('recording-list').children('recording').filter(function() { return $(this).attr('ext:score') == 100 }).attr('id')); }, 
+            dataType: 'xml'});
 }
