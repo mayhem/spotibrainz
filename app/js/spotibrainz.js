d41b1256aed494fee19235cde04e6fea261ac134
Robert Kaye <rob@musicbrainz.org>
2012-11-17 13:47:44 +0000

    Added gitignore Added jquery files Added change event received function

--- app/js/spotibrainz.js
+++ app/js/spotibrainz.js
@@ -1,8 +1,13 @@
 var sp = getSpotifyApi(1);
 var models, views;
 
+exports.init = init;
 function init() 
 {
     models = sp.require('sp://import/scripts/api/models');
     views = sp.require("sp://import/scripts/api/views");
+
+    models.player.observe(models.EVENT.CHANGE, function(event) {
+          console.log('Something changed!');
+    });
 }
